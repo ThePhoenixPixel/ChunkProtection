f88988512efac77a981b6761393ced8512521cba
BuildTools <unconfigured@null.spigotmc.org>
2023-09-30 09:01:17 +0200

    new

---
+++ src/main/java/eu/phoenixcraft/chunkprotection/ChunkProtection.java
@@ -0,0 +1,17 @@
+package eu.phoenixcraft.chunkprotection;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChunkProtection extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
